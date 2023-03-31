cd7489c1db3eedb9c521ab50622b39d13488512c
CircleCi <circleci@example.com>
2023-03-31 03:06:57 +0000

    [ci skip]

---
+++ assets/About-7f0d75c3.js
@@ -0,0 +1,1 @@
+import{a as t}from"./index-c0e77332.js";function r(){return t("div",{children:"About Page"})}export{r as default};
---
+++ assets/Home-d9308c19.js
@@ -0,0 +1,1 @@
+import{a as e}from"./index-c0e77332.js";function r(){return e("div",{children:"Home Page"})}export{r as default};
