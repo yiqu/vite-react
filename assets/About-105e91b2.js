5701124428915e1ebd4697a63041fd772b034565
CircleCi <circleci@example.com>
2023-04-03 03:31:36 +0000

    [ci skip]

--- assets/About-105e91b2.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./index-2a77d902.js";function r(){return t("div",{children:"About Page"})}export{r as default};
--- assets/Home-d7dd95cf.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./index-2a77d902.js";function r(){return e("div",{children:"Home Page"})}export{r as default};
