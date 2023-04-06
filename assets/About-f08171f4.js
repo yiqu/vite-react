44477d5c46f88fd780913d49f562a65963660799
CircleCi <circleci@example.com>
2023-04-06 04:46:05 +0000

    [ci skip]

---
+++ assets/About-f08171f4.js
@@ -0,0 +1,1 @@
+import{a as t}from"./index-e7a028fc.js";function r(){return t("div",{children:"About Page"})}export{r as default};
--- assets/Home-52e32244.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./index-337d8a9d.js";function r(){return e("div",{children:"Home Page"})}export{r as default};
